7f718c559db5707d7a79eea7bc04e752860020b0
levblanc <levblanc@gmail.com>
2016-07-21 15:31:23 +0800

    - 修改index.html引用的chunk-manifest.json文件名

--- src/index.html.js
+++ src/index.html.js
@@ -1,13 +1,16 @@
 module.exports = function (tplParams) {
-  var manifestSource = tplParams.compilation.assets['manifest.json'].source()
-  // remove manifest.json from assets so it won't be written to disk
-  delete tplParams.compilation.assets['manifest.json']
+  var manifestSource = tplParams.compilation.assets['chunk-manifest.json'].source()
+  // 把assets中的chunk-manifest.json移除，避免它被写到项目文件夹中
+  delete tplParams.compilation.assets['chunk-manifest.json']
 
   return `
     <!DOCTYPE html>
     <html>
       <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <title>枫车收银系统</title>
       </head>
       <body>
